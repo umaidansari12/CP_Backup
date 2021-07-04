d0cac37a6f1ec039946c5591eb6a0362912063ca
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-07-04 21:39:09 +0530

    Update rod_cutting.cpp

--- dp_problems/rod_cutting.cpp
+++ dp_problems/rod_cutting.cpp
@@ -8,6 +8,33 @@ using namespace std;
 
 class Solution {
 public:
+    int cutRodBottomUp(int price[], int n) {
+        //code here
+        int length[n] = {0};
+        for (int i = 0; i < n; i++)
+            length[i] = i + 1;
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= n; j++)
+            {
+                if (i == 0 or j == 0)
+                    dp[i][j] = 0;
+                else
+                {
+                    if (length[i - 1] <= j)
+                    {
+                        dp[i][j] = max(price[i - 1] + dp[i][j - length[i - 1]], dp[i - 1][j]);
+                    }
+                    else
+                    {
+                        dp[i][j] = dp[i - 1][j];
+                    }
+                }
+            }
+        }
+        return dp[n][n];
+    }
     int helper(int n, int rod, int price[], int length[], vector<vector<int>>&dp)
     {
         if (n == 0)
@@ -19,13 +46,29 @@ public:
         else
             return dp[n][rod] = helper(n - 1, rod, price, length, dp);
     }
-    int cutRod(int price[], int n) {
+    int cutRodTopDown(int price[], int n) {
         //code here
         int length[n] = {0};
         for (int i = 0; i < n; i++)
             length[i] = i + 1;
         vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
         return helper(n, n, price, length, dp);
+    }
+    int helperRecursion(int n, int rod, int price[], int length[])
+    {
+        if (n == 0)
+            return 0;
+        if (length[n - 1] <= rod)
+            return max(price[n - 1] + helperRecusion(n, rod - length[n - 1], price, length), helperRecursion(n - 1, rod, price, length));
+        else
+            return helperRecursion(n - 1, rod, price, length);
+    }
+    int cutRod(int price[], int n) {
+        //code here
+        int length[n] = {0};
+        for (int i = 0; i < n; i++)
+            length[i] = i + 1;
+        return helperRecursion(n, n, price, length);
     }
 };
 
