2c7a01a88b86cd0f8a1a3153b0e77c779ea6a4dc
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-07-05 01:54:30 +0530

    Variations of coin change problem created

---
+++ dp_problems/coin_change.cpp
@@ -0,0 +1,100 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    long long int countBottomUp( int S[], int m, int n )
+    {
+        //code here
+        vector<vector<long long int>> dp(m + 1, vector<long long int>(n + 1, -1));
+        for (int i = 0; i <= m; i++)
+        {
+            for (int j = 0; j <= n; j++)
+            {
+                if (i == 0 and j == 0)
+                {
+                    dp[i][j] = 1;
+                }
+                else if (i == 0)
+                {
+                    dp[i][j] = 0;
+                }
+                else if (j == 0)
+                {
+                    dp[i][j] = 1;
+                }
+                else
+                {
+                    if (S[i - 1] <= j)
+                    {
+                        dp[i][j] = dp[i][j - S[i - 1]] + dp[i - 1][j];
+                    }
+                    else
+                    {
+                        dp[i][j] = dp[i - 1][j];
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+
+    }
+    long long int countHelper(int S[], int m, int n , vector<vector<long long int>>&dp)
+    {
+        if (m == 0 and n == 0)
+        {
+            return 1;
+        }
+        if (m == 0)
+            return 0;
+        if (dp[m][n] != -1)
+            return dp[m][n];
+        if (S[m - 1] <= n)
+            return dp[m][n] = (long long int)countHelper(S, m - 1, n, dp) + (long long int)countHelper(S, m, n - S[m - 1], dp);
+        else
+            return dp[m][n] = (long long int)countHelper(S, m - 1, n, dp);
+    }
+    long long int countTopDown( int S[], int m, int n )
+    {
+        //code here
+        vector<vector<long long int>> dp(m + 1, vector<long long int>(n + 1, -1));
+        return countHelper(S, m, n, dp);
+
+    }
+    long long int count( int S[], int m, int n )
+    {
+        //code here.
+        if (m == 0 and n == 0)
+        {
+            return 1;
+        }
+        if (m == 0)
+            return 0;
+        if (S[m - 1] <= n)
+            return count(S, m - 1, n) + count(S, m, n - S[m - 1]);
+        else
+            return count(S, m - 1, n);
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        int arr[m];
+        for (int i = 0; i < m; i++)
+            cin >> arr[i];
+        Solution ob;
+        cout << ob.count(arr, m, n) << endl;
+    }
+
+
+    return 0;
+}  // } Driver Code Ends
---
+++ dp_problems/maximize_the_cut_segments.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maximizeTheCuts(int n, int x, int y, int z);
+
+int main() {
+
+    //taking testcases
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        //taking length of line segment
+        int n;
+        cin >> n;
+
+        //taking types of segments
+        int x, y, z;
+        cin >> x >> y >> z;
+
+        //calling function maximizeTheCuts()
+        cout << maximizeTheCuts(n, x, y, z) << endl;
+
+    }
+
+    return 0;
+}// } Driver Code Ends
+
+
+//Complete this function
+int rodCutting(vector<pair<int, int>>&rd, int n, int w, int i, vector<vector<int>>&dp)
+{
+    if (i >= n)
+        return 0;
+    if (w == 0)
+        return 0;
+    if (dp[i][w] != -1)
+        return dp[i][w];
+    if (rd[i].first <= w)
+    {
+        return (max(rd[i].second + rodCutting(rd, n, w - rd[i].first, i, dp), rodCutting(rd, n, w, i + 1, dp)));
+    }
+    else
+    {
+        return rodCutting(rd, n, w, i + 1, dp);
+    }
+}
+int maximizeTheCuts(int n, int x, int y, int z)
+{
+    //Your code here
+
+    /*vector<pair<int, int>> rd;
+    rd.push_back(make_pair(x, 1));
+    rd.push_back(make_pair(y, 1));
+    rd.push_back(make_pair(z, 1));
+    vector<vector<int>>dp(3, vector<int>(n + 1, -1));
+    return rodCutting(rd, 3, n, 0, dp);
+    vector<int>dp(n + 1, -1);*/
+    dp[0] = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i - x >= 0)
+            dp[i] = max(dp[i], dp[i - x]);
+        if (i - y >= 0)
+            dp[i] = max(dp[i], dp[i - y]);
+        if (i - z >= 0)
+            dp[i] = max(dp[i], dp[i - z]);
+        if (dp[i] != -1)
+            dp[i]++;
+    }
+    return max(dp[n], 0);
+}
