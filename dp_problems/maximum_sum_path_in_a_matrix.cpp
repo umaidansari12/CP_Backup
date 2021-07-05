3bc957445a1380ecf7249f04480b399360fd1a5f
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-07-05 20:16:00 +0530

    updated max_path_in_a_matrix.cpp

--- dp_problems/maximum_sum_path_in_a_matrix.cpp
+++ dp_problems/maximum_sum_path_in_a_matrix.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 using ll = long long;
 
-void maxPath(vector<vector<int>> a, int r, int c, int n, int sum, int &ans)
+void maxPath(vector<vector<int>> a, int r, int c, int n, int m, int sum, int &ans)
 {
 	ans = max(ans, sum);
-	if ((r + 1) < n and (c + 1) < n)
-		maxPath(a, r + 1, c + 1, n, sum + a[r + 1][c + 1], ans);
+	if ((r + 1) < n and (c + 1) < m)
+		maxPath(a, r + 1, c + 1, n, m, sum + a[r + 1][c + 1], ans);
 	if ((r + 1) < n and (c - 1) >= 0)
-		maxPath(a, r + 1, c - 1, n, sum + a[r + 1][c - 1], ans);
+		maxPath(a, r + 1, c - 1, n, m, sum + a[r + 1][c - 1], ans);
 }
 
-int maxPathBottomUp(vector<vector<int>>a, int n)
+int maxPathBottomUp(vector<vector<int>>a, int n, int m)
 {
 	vector<vector<int>> dp(n, vector<int>(n, 0));
 	if (n == 1)
@@ -22,21 +22,21 @@ int maxPathBottomUp(vector<vector<int>>a, int n)
 		dp[0][j] = a[0][j];
 	for (int i = 1; i < n; i++)
 	{
-		for (int j = 0; j < n; j++)
+		for (int j = 0; j < m; j++)
 		{
 			int maxSum = INT_MIN;
 			if ((j - 1) >= 0 and maxSum < dp[i - 1][j - 1])
 			{
 				maxSum = dp[i - 1][j - 1];
 			}
-			if ((j + 1) < n and maxSum < dp[i - 1][j + 1])
+			if ((j + 1) < m and maxSum < dp[i - 1][j + 1])
 			{
 				maxSum = dp[i - 1][j + 1];
 			}
 			dp[i][j] = maxSum + a[i][j];
 		}
 	}
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < m; i++)
 		ans = max(ans, dp[n - 1][i]);
 	return ans;
 }
@@ -52,20 +52,20 @@ int main()
 	{
 		int n, m, ans = 0;
 		cin >> n >> m;
-		vector<vector<int>> a(n, vector<int>(n, 0));
+		vector<vector<int>> a(n, vector<int>(m, 0));
 		for (int i = 0; i < n; i++)
 		{
-			for (int j = 0; j < n; j++)
+			for (int j = 0; j < m; j++)
 			{
 				cin >> a[i][j];
 			}
 		}
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < m; i++)
 		{
-			maxPath(a, 0, i, n, a[0][i], ans);
+			maxPath(a, 0, i, n, m, a[0][i], ans);
 		}
 		cout << ans << endl;
-		cout << maxPathBottomUp(a, n) << endl;
+		cout << maxPathBottomUp(a, n, m) << endl;
 	}
 
 
