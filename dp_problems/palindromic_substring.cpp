6d4af26f6e1af4c615a750cb5b178918d0217828
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-07-06 02:57:22 +0530

    Update palindromic_substring.cpp

--- dp_problems/palindromic_substring.cpp
+++ dp_problems/palindromic_substring.cpp
@@ -1,3 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int CountPS(char S[], int N);
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        char S[N + 1];
+        cin >> S;
+        cout << CountPS(S, N) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
 class Solution {
 public:
     int countSubstrings(string s) {
@@ -32,3 +54,37 @@ public:
         return count;
     }
 };
+
+
+int CountPS(char S[], int N)
+{
+    //code here
+    int count = 0;
+    bool dp[N][N];
+    memset(dp, 0, sizeof(dp));
+    for (int i = 0; i < N; i++)
+        dp[i][i] = true;
+    for (int i = 0; i < N - 1; i++)
+    {
+        if (S[i] == S[i + 1])
+        {
+            dp[i][i + 1] = true;
+            count++;
+        }
+    }
+    for (int k = 3; k <= N; k++)
+    {
+        for (int i = 0; i < (N - k + 1); i++)
+        {
+            int j = (i + k - 1);
+            if (S[i] == S[j] and dp[i + 1][j - 1])
+            {
+                count++;
+                dp[i][j] = true;
+            }
+        }
+    }
+    return count;
+
+
+}
