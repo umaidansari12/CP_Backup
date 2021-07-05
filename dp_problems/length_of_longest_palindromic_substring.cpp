ea7f38c33cc86975f3e896d7e80ad07ec045c5ee
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-07-06 03:17:03 +0530

    update length_of_longest_palindromic_substring.cpp

--- dp_problems/length_of_longest_palindromic_substring.cpp
+++ dp_problems/length_of_longest_palindromic_substring.cpp
@@ -34,6 +34,36 @@ string longestPalin (string s) {
 	return s.substr(start, ans);
 }
 
+int longestPalindrome(string s)
+{
+	int n = s.size(), maxLength = 1;
+	bool dp[n][n];
+	memset(dp, 0, sizeof(dp));
+	for (int i = 0; i < n; i++)
+		dp[i][i] = true;
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (s[i] == s[i + 1])
+		{
+			dp[i][i + 1] = true;
+			maxLength = max(maxLength, 2);
+		}
+	}
+	for (int k = 3; k <= n; k++)
+	{
+		for (int i = 0; i < (n - k + 1); i++)
+		{
+			int j = i + k - 1;
+			if (s[i] == s[j] and dp[i + 1][j - 1])
+			{
+				dp[i][j] = true;
+				maxLength = max(maxLength, k);
+			}
+		}
+	}
+	return maxLength;
+}
+
 int lengthOfLongestPalindromicSubstring(string s, int n)
 {
 	int ans = 0;
@@ -67,7 +97,7 @@ int main()
 		cin >> n;
 		string s;
 		cin >> s;
-		cout << lengthOfLongestPalindromicSubstring(s, n) << endl;
+		cout << longestPalindrome(s) << endl;
 	}
 
 
