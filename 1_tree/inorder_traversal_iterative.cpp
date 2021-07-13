a93016727b6da1d0281cc3e7403432be8c14916c
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-07-14 00:55:37 +0530

    Update inorder_traversal_iterative.cpp

--- 1_tree/inorder_traversal_iterative.cpp
+++ 1_tree/inorder_traversal_iterative.cpp
@@ -101,6 +101,70 @@ public:
         stack<Node*> s;
         Node* current = root;
         vector<int> res;
+        /*   1
+           /    \
+          2      3
+        /   \
+        4     5
+
+        Step 1 Creates an empty stack: S = NULL
+
+        Step 2 sets current as address of root: current -> 1
+
+        Step 3 Pushes the current node and set current = current->left
+        until current is NULL
+        current -> 1
+        push 1: Stack S -> 1
+        current -> 2
+        push 2: Stack S -> 2, 1
+        current -> 4
+        push 4: Stack S -> 4, 2, 1
+        current = NULL
+
+        Step 4 pops from S
+        a) Pop 4: Stack S -> 2, 1
+        b) print "4"
+        c) current = NULL /*right of 4 */ //and go to step 3
+        /* Since current is NULL step 3 doesn't do anything.
+
+         Step 4 pops again.
+         a) Pop 2: Stack S -> 1
+         b) print "2"
+         c) current -> 5/*right of 2 */ //and go to step 3
+
+        /*Step 3 pushes 5 to stack and makes current NULL
+        Stack S -> 5, 1
+        current = NULL
+
+        Step 4 pops from S
+        a) Pop 5: Stack S -> 1
+        b) print "5"
+        c) current = NULL /*right of 5 */ //and go to step 3
+        //Since current is NULL step 3 doesn't do anything*/*/
+
+        /*  Step 4 pops again.
+        a) Pop 1: Stack S -> NULL
+          b) print "1"
+          c) current -> 3 /*right of 1
+
+          Step 3 pushes 3 to stack and makes current NULL
+          Stack S -> 3
+          current = NULL
+
+                    Step 4 pops from S
+        a) Pop 3: Stack S -> NULL
+          b) print "3"
+          c) current = NULL*/ /*right of 3 */
+
+        //Traversal is done now as stack S is empty and current is NULL.* /
+        // if current is NULL or stack is empty then we will come out of our
+        // loop but if not then we will move in the left branch until it
+        // becomes NULL and keeps pushing its data if current is NULL left
+        // branch exhausted then we will move our current to right if the
+        // stack  is not empty means many elements are waiting to get printed
+        // so we will pick the top element that will be the parent node from
+        // where current has come and set current as its right
+
         while (current or !s.empty())
         {
             while (current)
@@ -110,8 +174,12 @@ public:
             }
             if (!s.empty())
             {
-                res.push_back(s.top()->data);
+                /*res.push_back(s.top()->data);
                 current = s.top()->right;
+                s.pop();*/
+                current = s.top();
+                res.push_back(current->data);
+                current = current->right;
                 s.pop();
             }
         }
