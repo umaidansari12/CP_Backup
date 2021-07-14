f2b9f680b863af1a4dcc893af4b0094e8f8f782d
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-07-14 20:53:47 +0530

    Update 158_A_next_round.cpp

--- codeforces/800/158_A_next_round.cpp
+++ codeforces/800/158_A_next_round.cpp
@@ -12,15 +12,25 @@ int solve()
 	{
 		cin >> arr[i];
 	}
-
+	// int value = arr[k - 1];
+	// sort(arr, arr + n);
+	// int zero_position = 0;
+	// while (arr[zero_position] == 0)
+	// 	zero_position++;
+	// if (zero_position == n)
+	// 	return 0;
+	// int value_position = lower_bound(arr, arr + n, value) - arr;
+	// //cout << value_position << " " << zero_position << endl;
+	// return n - (value_position + zero_position);
 	/*
 	for (int i = 0; i < n; i++)
 	{
 		if (arr[i] > 0 and arr[i] >= arr[k - 1])
 			ans++;
 	}
-	*/
+
 	return ans;
+	*/
 }
 
 int main()
