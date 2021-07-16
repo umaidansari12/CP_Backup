227757cd15ccd6ee2a75811c3f954f76b007de28
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-07-16 17:59:33 +0530

    Update split_bst.cpp

--- 1_tree/split_bst.cpp
+++ 1_tree/split_bst.cpp
@@ -148,3 +148,111 @@ int main()
 
 	return 0;
 }
+
+/*
+    Time Complexity: O(N)
+    Space Complexity: O(N)
+
+    Where N is the total number of nodes in the given tree.
+*/
+
+// Recursive function to split the given tree into two trees.
+vector<TreeNode<int>* > splitBST(TreeNode<int>* root, int value)
+{
+	// Initialize vector of size two with 'NULL' nodes of 'first' and 'second' trees.
+	vector<TreeNode<int>* > ret(2, NULL);
+
+	// Base case
+	if (root == NULL)
+	{
+		return ret;
+	}
+
+	/*
+	 If the value of root node is less than or equal to given value,
+	 then add left subtree along with root node to the 'first' tree.
+	 and recursively call function for right subtree. And if value of
+	 root node is greator than given value, then add right subtree along with
+	 root node to 'second' tree ans recursively call function for left subtree.
+	*/
+	if (root -> val <= value)
+	{
+		ret[0] = root;
+		vector<TreeNode<int>* > temp = splitBST(root -> right, value);
+		root -> right = temp[0];
+		ret[1] = temp[1];
+	}
+	else
+	{
+		ret[1] = root;
+		vector<TreeNode<int>* > temp = splitBST(root -> left, value);
+		root -> left = temp[1];
+		ret[0] = temp[0];
+	}
+
+	// Return vector of root nodes of 'first' and 'second' trees.
+	return ret;
+}
+
+/*
+    Time Complexity: O(N)
+    Space Complexity: O(1)
+
+    Where N is the total number of nodes in the given tree.
+*/
+
+vector<TreeNode<int>* > splitBST (TreeNode<int>* root, int value)
+{
+	// Initialize vector of size two with node of 'first' and 'second' trees.
+	vector<TreeNode<int>* > afterSplit(2, new TreeNode<int>(0));
+
+	// Declare a current node variables for the first and second trees to keep track for adding next node.
+	TreeNode<int>* currFirst = afterSplit[0];
+	TreeNode<int>* currSecond = afterSplit[1];
+
+	// Run a loop until we process whole tree.
+	while (root != NULL)
+	{
+		/*
+		If value of current root is less than or equal to given value,
+		then add a new node with value equal to root's value to the right child of 'currFirst',
+		as we are assuming that left subtree is already been added. Then move 'currFirst' to
+		its right child and add left subtree of root node.
+		*/
+		if (root -> val <= value)
+		{
+			currFirst -> right = new TreeNode<int>(root -> val);
+			currFirst = currFirst -> right;
+			currFirst -> left  = root ->left;
+
+			// Move root node to its right child to explore right subtree.
+			root = root -> right;
+		}
+
+		/*
+		If value of current root is greater than given value,
+		then add a new node with value equal to root's value to the left child of 'currSecond',
+		as we are assuming that right subtree is already been added. Then move 'currSecond' to
+		its left child and add right subtree of root node.
+		*/
+		else
+		{
+			currSecond -> left = new TreeNode<int> (root -> val);
+			currSecond = currSecond -> left;
+			currSecond -> right = root -> right;
+
+			// Move root node to left subtree.
+			root  = root -> left;
+		}
+	}
+
+	/*
+	 The root node of 'first' and 'second' trees are added as right child and left child
+	 of 'afterSplit[0]' and 'afterSplit[1]' respectively. So we will return
+	 'afterSplit[0] -> right' and 'afterSplit[1] -> left'.
+	*/
+	afterSplit[0] = afterSplit[0] -> right;
+	afterSplit[1] = afterSplit[1] -> left;
+
+	return afterSplit;
+}
