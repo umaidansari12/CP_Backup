7bf8a80befef0d0d0b991083b1dc071c29e650cf
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-07-18 01:29:13 +0530

    Update minimum_number_of_jumps.cpp

--- 6_dp/minimum_number_of_jumps.cpp
+++ 6_dp/minimum_number_of_jumps.cpp
@@ -20,6 +20,26 @@ int minNumberofJumps(vector<int>&a, int i, int n)
 	return ans;
 }
 
+int minNumberofJumpsTopDown(vector<int>&a, int i, int n, vector<int>&dp)
+{
+	if (i == n - 1)
+		return 0;//means we have reached end of the array and we need 0 steps to reach here
+	if (i >= n)
+		return INT_MAX;//reached outside the boundary so return infinity
+	if (dp[i] != -1)
+		return dp[i];
+	int steps = INT_MAX, maxJumps = a[i];
+	for (int jump = 1; jump <= maxJumps; jump++)
+	{
+		int next_idx = i + jump;//next index would be current index + jump
+		int subProblem = minNumberofJumpsTopDown(a, next_idx, n, dp);
+		if (subProblem != INT_MAX)
+			steps = min(steps, 1 + subProblem);
+	}
+	dp[i] = steps;
+	return dp[i];
+}
+
 int main()
 {
 	ios_base :: sync_with_stdio(false);
@@ -31,10 +51,10 @@ int main()
 	{
 		int n;
 		cin >> n;
-		vector<int> a(n);
+		vector<int> a(n), dp(n, -1);
 		for (int i = 0; i < n; i++)
 			cin >> a[i];
-		cout << minNumberofJumps(a, 0, n) << endl;
+		cout << minNumberofJumpsTopDown(a, 0, n, dp) << endl;
 	}
 
 	return 0;
