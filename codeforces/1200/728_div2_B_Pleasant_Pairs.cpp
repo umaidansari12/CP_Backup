87bffa51e908f85da004c9cf0ab9f992d02d58e9
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-07-18 00:54:00 +0530

    Update 728_div2_B_Pleasant_Pairs.cpp

--- codeforces/1200/728_div2_B_Pleasant_Pairs.cpp
+++ codeforces/1200/728_div2_B_Pleasant_Pairs.cpp
@@ -5,19 +5,44 @@ using ll = long long;
 
 int solve()
 {
-	int n, cnt = 0;
+	int n;
+	ll cnt = 0;
 	cin >> n;
-	ll a[n];
-	for (int i = 0; i < n; i++)
-		cin >> a[i];
-	for (int i = 0; i < n - 1; i++)
+	ll a[n + 1];
+	pair<ll, ll> br[n + 1];
+	for (int i = 1; i <= n; i++)
 	{
-		for (int j = i + 1; j < n; j++)
+		cin >> a[i];
+		br[i] = {a[i], i};
+	}
+	sort(br + 1, br + n + 1);
+	// for (int i = 0; i < n - 1; i++)
+	// {
+	// 	for (int j = i + 1; j < n; j++)
+	// 	{
+	// 		if ((a[i]*a[j]) == (i + j + 2))
+	// 			cnt++;
+	// 	}
+	// }
+	for (ll i = 1; i <= n; i++)
+	{
+		for (ll j = i + 1; j <= n; j++)
 		{
-			if ((a[i]*a[j]) == (i + j + 2))
+			ll L = br[i].second + br[j].second;
+			ll R = br[j].first * br[i].first;
+			if (L == R)
 				cnt++;
+			if (R > (2 * n))
+				break;
 		}
 	}
+	/*unordered_map<int, int> m;
+	for (int i = 0; i < n; i++)
+	{
+		if (m.find(a[i] - i) != m.end())
+			cnt += m[a[i] - i];
+		m[(i + 2) / a[i]]++;
+	}*/
 	return cnt;
 }
 
