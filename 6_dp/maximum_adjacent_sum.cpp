d3d5a6c5cce878026ce880f753bd7d2e0c43ac65
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-07-20 17:24:59 +0530

    Update maximum_adjacent_sum.cpp

--- 6_dp/maximum_adjacent_sum.cpp
+++ 6_dp/maximum_adjacent_sum.cpp
@@ -7,7 +7,7 @@ int maximumAdjacentSum(vector<int>&a, int n)
 {
 	vector<int> dp(n, 0);
 	dp[0] = a[0];
-	dp[1] = max(dp[0], dp[1]);
+	dp[1] = max(a[0], a[1]);
 	for (int i = 2; i < n; i++)
 	{
 		dp[i] = max(dp[i - 2] + a[i], dp[i - 1]);
