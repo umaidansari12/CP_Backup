d7370c0ef21e4f83eba0de94f570a3a70cdbe157
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-07-23 01:35:57 +0530

    Update unique_binary_search_trees.cpp

--- 6_dp/unique_binary_search_trees.cpp
+++ 6_dp/unique_binary_search_trees.cpp
@@ -60,3 +60,22 @@ public:
         return dp[n];
     }
 };
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            dp[i] = 0;
+            for (int j = 1; j <= i; j++)
+            {
+                dp[i] += (dp[j - 1] * dp[i - j]);
+            }
+        }
+
+        return dp[n];
+    }
+};
