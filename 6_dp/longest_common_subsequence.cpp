41cbee3ea38e078d5be9abc4e95c417c694a979b
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-07-24 18:56:36 +0530

    Update longest_common_subsequence.cpp

--- 6_dp/longest_common_subsequence.cpp
+++ 6_dp/longest_common_subsequence.cpp
@@ -49,7 +49,27 @@ public:
 int longestCommonSubsequenceBottomUp(string s1, string s2, int n, int m)
 {
 	vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-	dp[0][0] = 1;
+	dp[0][0] = 0;
+	for (int i = 0; i <= n; i++)
+	{
+		for (int j = 0; j <= m; j++)
+		{
+			if (i == 0 or j == 0)
+				dp[i][j] = 0;
+			else
+			{
+				if (s1[i - 1] == s2[j - 1])
+				{
+					dp[i][j] = 1 + dp[i - 1][j - 1];
+				}
+				else
+				{
+					dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+				}
+			}
+		}
+	}
+	return dp[n][m];
 }
 
 int main()
@@ -65,7 +85,8 @@ int main()
 		cin >> s1 >> s2;
 		vector<vector<int>> dp(s1.size(), vector<int>(s2.size(), -1));
 		//cout << longestCommonSubsequence(s1, s2, s1.size(), s2.size()) << endl;
-		cout << longestCommonSubsequenceTopDown(s1, s2, s1.size(), s2.size(), dp) << endl;
+		//cout << longestCommonSubsequenceTopDown(s1, s2, s1.size(), s2.size(), dp) << endl;
+		cout << longestCommonSubsequenceBottomUp(s1, s2, s1.size(), s2.size()) << endl;
 	}
 
 
