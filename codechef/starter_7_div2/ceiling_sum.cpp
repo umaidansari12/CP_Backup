5f20f56035bd445da6f57b40b37b6566d59a0658
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-07-26 03:11:35 +0530

    Update ceiling_sum.cpp

--- codechef/starter_7_div2/ceiling_sum.cpp
+++ codechef/starter_7_div2/ceiling_sum.cpp
@@ -17,17 +17,31 @@ int main()
 		int l = min(a, b);
 		int r = max(a, b);
 		//int ans = (b - a) / 2;
-		int ans = INT_MIN;
-
-		int lsum = (b - (l + 1)) / 2;
-		int rsum = ()
-		           // for (int x = l; x <= r; x++)
-		           // {
-		           // 	int lsum = ceil((b - x) / (float)2);
-		           // 	int rsum = ceil((x - a) / (float)2);
-		           // 	ans = max(ans, rsum + lsum);
-		           // }
-		           cout << ceil(ans) << endl;
+		//int ans = INT_MIN;
+		// int lsum = (b - (l + 1)) / 2;
+		// int rsum = ();
+		// for (int x = l; x <= r; x++)
+		// {
+		// 	int lsum = ceil((b - x) / (float)2);
+		// 	int rsum = ceil((x - a) / (float)2);
+		// 	ans = max(ans, rsum + lsum);
+		// }
+		if (a == b)
+			cout << 0 << endl;
+		else if (a < b)
+		{
+			// if (abs(a - b) % 2 == 0)
+			// 	cout << 1 << endl;
+			// else
+			cout << ((b - a) / 2) + 1 << endl;
+		}
+		else if (a > b)
+		{
+			if ((a - b) % 2 == 0)
+				cout << ((b - a) / 2) + 1 << endl;
+			else
+				cout << ((b - a) / 2) << endl;
+		}
 	}
 
 
