07b6f2a8888bb049742bd2c77e517b72deb9bf45
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-07-27 14:51:33 +0530

    Update connect_nodes_at_same_level.cpp

--- 1_tree/connect_nodes_at_same_level.cpp
+++ 1_tree/connect_nodes_at_same_level.cpp
@@ -190,3 +190,103 @@ int main()
     return 0;
 }
 // } Driver Code Ends
+
+/*
+    Time complexity: O(N)
+    Space complexity: O(1)
+
+    Where 'N' is the number of nodes in a binary tree.
+*/
+
+void connectNodes(BinaryTreeNode<int> *root) {
+
+    // Keep the address of the first node of the current level.
+    BinaryTreeNode<int> *startNode = root;
+
+    while (startNode != NULL) {
+
+        BinaryTreeNode<int> *ptr = startNode;
+        BinaryTreeNode<int> *previous = NULL;
+        startNode = NULL;
+
+        // Traversing over nodes of current level and populating 'next' pointer of nodes of next level.
+        while (ptr != NULL) {
+
+            if (ptr->left != NULL) {
+
+                if (previous != NULL) {
+                    previous->next = ptr->left;
+                }
+
+                // Update 'startNode' with first node of next level if not already done.
+                if (startNode == NULL) {
+                    startNode = ptr->left;
+                }
+
+                // Update previous pointer
+                previous = ptr->left;
+            }
+
+            if (ptr->right != NULL) {
+
+                if (previous != NULL) {
+                    previous->next = ptr->right;
+                }
+
+                // Update 'startNode' with first node of next level if not already done.
+                if (startNode == NULL) {
+                    startNode = ptr->right;
+                }
+
+                // Update previous pointer.
+                previous = ptr->right;
+            }
+
+            ptr = ptr->next;
+        }
+    }
+}
+
+/*
+    Time complexity: O(N)
+    Space complexity: O(N)
+
+    Where 'N' is the number of nodes in a binary tree.
+*/
+
+#include <queue>
+
+void connectNodes(BinaryTreeNode<int> *root) {
+
+    // Create queue and enqueue address of root in it.
+    queue< BinaryTreeNode<int>* > que;
+    que.push(root);
+
+    // Number of nodes in current level.
+    int nodesCount = 1;
+
+    while (!que.empty()) {
+        BinaryTreeNode<int> *previous = NULL;
+
+        // Traversing over nodes of current level.
+        while (nodesCount > 0) {
+            if (previous != NULL) {
+                previous->next = que.front();
+            }
+            previous = que.front();
+
+            // Pushing left and right child of current node in queue.
+            if (que.front()->left != NULL) {
+                que.push(que.front()->left);
+            }
+            if (que.front()->right != NULL) {
+                que.push(que.front()->right);
+            }
+            que.pop();
+            nodesCount--;
+        }
+
+        // Updating number of nodes in current level.
+        nodesCount = que.size();
+    }
+}
