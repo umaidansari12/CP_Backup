3373b3620f05ceac3b9ff975ace7870b9ff0e3a2
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-07-30 18:01:51 +0530

    Create foo_and_exams.cpp

---
+++ 13_binary_search/foo_and_exams.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+ll a, b, c, d, k;
+
+ll F(ll mid, ll a, ll b, ll c, ll d)
+{
+	return (a * mid * mid * mid) + (b * mid * mid) + (c * mid) + d;
+}
+
+ll BSearch1(ll a, ll b, ll c, ll d, ll k)
+{
+	ll low = 0 , high = ceil(sqrt(k)), ans = 0, mid;
+	high = 799000;
+	//since ((10^6)^3) = 10^18
+
+	//so upperbound for t is 10^6
+	while (low <= high)
+	{
+		mid = low + (high - low) / 2;
+		ll F1 = F(mid, a, b, c, d);
+		ll F2 = F(mid + 1, a, b, c, d);
+		if (F1 <= k and F2 > k)
+		{
+			return mid;
+			// ans = mid;
+			// low = mid + 1;
+		}
+		else if (F1 <= k)
+			low = mid + 1;
+		else if (F1 > k)
+			high = mid - 1;
+	}
+	return ans;
+}
+
+
+ll BSearch(ll a, ll b, ll c, ll d, ll k)
+{
+	ll low = 0 , high = ceil(sqrt(k)), ans = 0, mid;
+	while (low <= high)
+	{
+		mid = low + (high - low) / 2;
+		ll F1 = F(mid, a, b, c, d);
+		//ll F2 = F(mid + 1, a, b, c, d);
+		if (F1 <= k)
+		{
+			//return mid;
+			ans = mid;
+			low = mid + 1;
+		}
+		// else if (F1 < k)
+		// 	low = mid + 1;
+		else if (F1 > k)
+			high = mid - 1;
+	}
+	return ans;
+}
+
+int main()
+{
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		cin >> a >> b >> c >> d >> k;
+		//cout << a << " " << b << " " << c << " " << d << " " << k << '\n';
+		// ll low = 1, high = ceil(sqrt(k)), ans = 0;
+		// for (ll i = low; i <= high; i++)
+		// {
+		// 	if (F(i + 1, a, b, c, d) > k and F(i, a, b, c, d) <= k)
+		// 	{
+		// 		ans = i;
+		// 		break;
+		// 	}
+		// }
+		// cout << ans << '\n';
+		cout << BSearch(a, b, c, d, k) << '\n';
+	}
+
+
+	return 0;
+}
