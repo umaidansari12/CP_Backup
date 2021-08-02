7873ba1fa14b2bf2bfce7d325734af0b71fdaa77
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-08-02 18:03:41 +0530

    Update megaprime_numbers.cpp

--- 9_maths_and_number_theory/megaprime_numbers.cpp
+++ 9_maths_and_number_theory/megaprime_numbers.cpp
@@ -32,7 +32,43 @@ bool isDigitsPrime(long n)
     return true;
 }
 
+bool ismega(long long int n)
+{
+    bool flag = false;
+    int temp;
+    while (n != 0)
+    {
+        temp = n % 10;
+        if (temp == 1)
+        {
+            flag = true;
+            break;
+        }
+        n = n / 10;
+        for (int j = 2; j <= (temp / 2); j++)
+        {
+            if (temp % j == 0)
+            {
+                flag = true;
+                break;
+            }
 
+        }
+        if (flag)
+        {
+            break;
+        }
+    }
+    if (flag)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+
+}
 
 
 int main()
@@ -46,18 +82,45 @@ int main()
 
     long last = stol(first_multiple_input[1]);
 
-    long cnt = 0;
+    // long cnt = 0;
+    // bool flag = false;
+
+
+    // if (first == 1)
+    //     first = 2;
+    // for (int i = first; i <= last; i++)
+    // {
+    //     for (int j = 2; j <= (i / 2); j++)
+    //     {
+    //         if (i % j == 0)
+    //         {
+    //             flag = true;
+    //             break;
+    //         }
+
+    //     }
+
+    //     if (!flag)
+    //     {
+    //         if (ismega(i))
+    //         {
+    //             cnt += i;
+    //             //cout << i << " ";
+    //         }
+    //     }
+    //     flag = false;
+    // }
 
     // Write your code here
-    for (long number = first; number <= last; number++)
-    {
-        if (number == 1 or number == 0)
-            continue;
-        if (isPrime(number) and isDigitsPrime(number))
-        {
-            cnt++;
-        }
-    }
+    // for (long number = first; number <= last; number++)
+    // {
+    //     if (number == 1 or number == 0)
+    //         continue;
+    //     if (isPrime(number) and isDigitsPrime(number))
+    //     {
+    //         cnt++;
+    //     }
+    // }
     cout << cnt << '\n';
 
     return 0;
