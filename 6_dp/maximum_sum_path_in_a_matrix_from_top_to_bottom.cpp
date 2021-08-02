9addd487dcd9d9fb74727c3a18a5bf41398c6933
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-08-02 22:00:47 +0530

    Create maximum_sum_path_in_a_matrix_from_top_to_bottom.cpp

---
+++ 6_dp/maximum_sum_path_in_a_matrix_from_top_to_bottom.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int dx[] = {1, 1};
+int dy[] = { -1, 1};
+
+bool isValid(int row, int col, int n, int m)
+{
+	if ((row >= 0 and row < n) and (col >= 0 and col < m))
+		return true;
+	return false;
+}
+
+
+int MaxPathSumRecurHelper(vector<vector<int>>&a, int row, int col, int n, int m)
+{
+	if (row == n - 1)
+		return a[row][col];
+
+	int maxSum = INT_MIN;
+	for (int i = 0; i < 2; i++)
+	{
+		int nextRow = row + dx[i];
+		int nextCol = col + dy[i];
+		if (isValid(nextRow, nextCol, n, m))
+			maxSum = max(maxSum, MaxPathSumRecurHelper(a, nextRow, nextCol, n, m));
+	}
+	return maxSum + a[row][col];
+}
+
+int MaxPathSumRecur(vector<vector<int>>&a, int n, int m)
+{
+	int ans = INT_MIN;
+	for (int i = 0; i < n; i++)
+		ans = max(ans, MaxPathSumRecurHelper(a, 0, i, n, m));
+	return ans;
+}
+
+int MaxPathSumTopDownHelper(vector<vector<int>>&a, int row, int col, int n, int m, vector<vector<int>>&dp)
+{
+	if (row == n - 1)
+		return a[row][col];
+
+	if (dp[row][col] != INT_MIN)
+		return dp[row][col];
+
+	int maxSum = INT_MIN;
+	for (int i = 0; i < 2; i++)
+	{
+		int nextRow = row + dx[i];
+		int nextCol = col + dy[i];
+		if (isValid(nextRow, nextCol, n, m))
+			maxSum = max(maxSum, MaxPathSumTopDownHelper(a, nextRow, nextCol, n, m, dp));
+	}
+	return dp[row][col] = maxSum + a[row][col];
+}
+
+int MaxPathSumTopDown(vector<vector<int>>&a, int n, int m)
+{
+	int ans = INT_MIN;
+	vector<vector<int>> dp(n, vector<int>(m, INT_MIN));
+	for (int i = 0; i < n; i++)
+		ans = max(ans, MaxPathSumTopDownHelper(a, 0, i, n, m, dp));
+	return ans;
+}
+
+int MaxPathSumBottomUp(vector<vector<int>>&a, int n, int m)
+{
+	for (int row = 1; row < n; row++)
+	{
+		for (int col = 0; col < m; col++)
+		{
+			int maxValue = INT_MIN;
+			if (isValid(row - 1, col - 1, n, m))
+				maxValue = max(maxValue, a[row - 1][col - 1]);
+			if (isValid(row - 1, col + 1, n, m))
+				maxValue = max(maxValue, a[row - 1][col + 1]);
+			a[row][col] = a[row][col] + maxValue;
+		}
+	}
+	int ans = INT_MIN;
+
+	for (int i = 0; i < m; i++)
+		ans = max(ans, a[n - 1][i]);
+	return ans;
+}
+
+
+int main()
+{
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>> a(n, vector<int>(m, 0));
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				cin >> a[i][j];
+			}
+		}
+
+
+		cout << MaxPathSumBottomUp(a, n, m) << '\n';
+
+	}
+
+
+
+	return 0;
+}
