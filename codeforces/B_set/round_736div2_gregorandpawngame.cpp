fbfb7fe116084ff2180a42ef42f10238938dd77e
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-08-04 14:52:12 +0530

    Update round_736div2_gregorandpawngame.cpp

--- codeforces/B_set/round_736div2_gregorandpawngame.cpp
+++ codeforces/B_set/round_736div2_gregorandpawngame.cpp
@@ -34,6 +34,39 @@ ll solve()
 
 }
 
+void solveEditorial()
+{
+	ll n, cnt = 0;
+	cin >> n;
+	string s1, s2;
+	cin >> s1 >> s2;
+	vector<bool> taken(n);
+	for (ll i = 0; i < n; i++)
+	{
+		if (s2[i] == '1')
+		{
+			for (int j = i - 1; j <= i + 1; j++)
+			{
+				if (j >= 0 and j < n)
+				{
+					if (!taken[j])
+					{
+						if ((s1[j] == '1' and i != j) or (s1[j] == '0' and i == j))
+						{
+							taken[j] = true;
+							cnt++;
+							break;
+						}
+					}
+				}
+
+			}
+		}
+	}
+
+	cout << cnt << '\n';
+}
+
 int main()
 {
 	ios_base :: sync_with_stdio(false);
@@ -42,7 +75,8 @@ int main()
 	int t;
 	cin >> t;
 	while (t--) {
-		cout << solve() << '\n';
+		//cout << solve() << '\n';
+		solveEditorial();
 	}
 
 
