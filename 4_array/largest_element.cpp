eeb59ab006d768380bf3d5effc566634606df819
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-08-05 02:16:30 +0530

    Create largest_element.cpp

---
+++ 4_array/largest_element.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int largestElement(vector<int> arr) {
+    int largest = INT_MIN;
+    for (int i = 0; i < arr.size(); i++)
+        largest = max(largest, arr[i]);
+    return largest;
+
+}
