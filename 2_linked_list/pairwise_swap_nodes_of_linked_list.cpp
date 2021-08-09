062caee3591fad03243945177875588f5bb4cdba
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-08-09 15:28:28 +0530

    Update pairwise_swap_nodes_of_linked_list.cpp

--- 2_linked_list/pairwise_swap_nodes_of_linked_list.cpp
+++ 2_linked_list/pairwise_swap_nodes_of_linked_list.cpp
@@ -100,3 +100,29 @@ int main()
         cout << endl;
     }
 }  // } Driver Code Ends
+
+ListNode* Solution::swapPairs(ListNode* A) {
+    if (!A)
+        return NULL;
+    ListNode* head = A;
+    ListNode* curr, *next, *prev;
+    prev = NULL;
+    curr = A;
+    next = curr->next;
+    if (next)
+        A = next;
+    while (next)
+    {
+        curr->next = next->next;
+        next->next = curr;
+        if (prev)
+            prev->next = next;      //I had missed this part of linking 1 to 4: 2->1 ~ 4->3
+        prev = curr;
+        curr = curr->next;
+        if (curr)
+            next = curr->next;
+        else
+            next = NULL;
+    }
+    return A;
+}
