3f4e3f6a0c1f73b75686e380ce9d5f0e49ac14d1
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-08-11 15:36:39 +0530

    Create longest_bitonic_sequence.cpp

---
+++ 6_dp/longest_bitonic_sequence.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+	int LongestBitonicSequence(vector<int>A)
+	{
+		// code here
+		int n = A.size();
+		int lis[n];
+		int lds[n];
+		for (int i = 0; i < n; i++) {
+			lis[i] = 1;
+			lds[i] = 1;
+		}
+		for (int i = 1; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				if (A[i] > A[j] && lis[i] < lis[j] + 1)
+					lis[i] = lis[j] + 1;
+			}
+		}
+		for (int i = n - 2; i >= 0; i--) {
+			for (int j = n - 1; j > i; j--) {
+				if (A[i] > A[j] && lds[i] < lds[j] + 1)
+					lds[i] = lds[j] + 1;
+			}
+		}
+		int ans = 0;
+		for (int i = 0; i < n; i++)
+			ans = max(ans, lis[i] + lds[i] - 1);
+		return ans;
+	}
+};
+
+// { Driver Code Starts.
+int main() {
+	int tc;
+	cin >> tc;
+	while (tc--) {
+		int n;
+		cin >> n;
+		vector<int>nums(n);
+		for (int i = 0; i < n; i++)
+			cin >> nums[i];
+		Solution ob;
+		int ans = ob.LongestBitonicSequence(nums);
+		cout << ans << "\n";
+	}
+	return 0;
+}  // } Driver Code Ends
