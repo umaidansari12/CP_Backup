6e452b3fedc9b1e154710db1d23961ba8d04b7f6
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-08-16 23:59:18 +0530

    Update course_schedule.cpp

--- 8_graph/course_schedule.cpp
+++ 8_graph/course_schedule.cpp
@@ -72,3 +72,75 @@ public:
 
     }
 };
+
+class Solution {
+public:
+    bool dfs(vector<int>g[], vector<bool>&vis, vector<bool>&rec, int node)
+    {
+        vis[node] = true;
+        rec[node] = true;
+        for (auto i : g[node])
+        {
+            if (!vis[i] && dfs(g, vis, rec, i))
+                return true;
+            else if (rec[i])
+                return true;
+        }
+        rec[node] = false;
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> g[numCourses];
+        for (int i = 0; i < prerequisites.size(); i++)
+        {
+            g[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+        vector<bool> vis(numCourses, false);
+        vector<bool> res(numCourses, false);
+        for (int i = 0; i < numCourses; i++)
+        {
+            if (!vis[i] and dfs(g, vis, res, i))
+            {
+                return false;
+            }
+        }
+        return true;
+
+    }
+};
+
+class Solution {
+public:
+    bool dfs(vector<int>g[], vector<bool>&vis, vector<bool>&rec, int node)
+    {
+        vis[node] = true;
+        rec[node] = true;
+        for (auto i : g[node])
+        {
+            if (!vis[i] && dfs(g, vis, rec, i))
+                return true;
+            else if (rec[i])
+                return true;
+        }
+        rec[node] = false;
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> g[numCourses];
+        for (int i = 0; i < prerequisites.size(); i++)
+        {
+            g[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+        vector<bool> vis(numCourses, false);
+        vector<bool> res(numCourses, false);
+        for (int i = 0; i < numCourses; i++)
+        {
+            if (!vis[i] and dfs(g, vis, res, i))
+            {
+                return false;
+            }
+        }
+        return true;
+
+    }
+};
