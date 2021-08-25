8f7a85b07fd83c6b6a34abf54ae071c4908d791b
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-08-25 22:14:18 +0530

    Create count_the_zeroes.cpp

---
+++ 4_array/count_the_zeroes.cpp
@@ -0,0 +1,34 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+public:
+    int countZeroes(int arr[], int n) {
+        // code here
+        return count(arr, arr + n, 0);
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.countZeroes(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
