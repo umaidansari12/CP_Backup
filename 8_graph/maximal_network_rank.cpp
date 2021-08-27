d0d42988a5ce13202fcfc87b7ee063707fc600a0
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-08-28 01:46:05 +0530

    Update maximal_network_rank.cpp

--- 8_graph/maximal_network_rank.cpp
+++ 8_graph/maximal_network_rank.cpp
@@ -29,3 +29,29 @@ public:
 
 // above is wrong solution
 
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        vector<vector<bool>> connected(n, vector<bool>(n, false)); //we use a 2-D representation of the graph to check if 2 vertices are connected with a direct road or not
+        vector<int> degree(n, 0); //we are calculating degree of every node
+        for (auto road : roads)
+        {
+            int u = road[0];
+            int v = road[1];
+            degree[u]++;
+            degree[v]++;
+            connected[u][v] = true;
+            connected[v][u] = true;
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                ans = max(ans, degree[i] + degree[j] - (connected[i][j] ? 1 : 0)); //taking sum of degree and if they are connected subtracting 1 from it else 0
+            }
+        }
+        return ans;
+    }
+};
+
