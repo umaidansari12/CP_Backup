532ae96ef1fbeb1be8b1b2dd0d4dd6b1fdfb8fa7
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-09-11 13:44:59 +0530

    Update B_say_no_to_drugs.cpp

--- codechef/algomaniac_div3/B_say_no_to_drugs.cpp
+++ codechef/algomaniac_div3/B_say_no_to_drugs.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 using ll = long long;
 
-void solve() {
+void solve1() {
 	long long int n, mx = -1001, k, l, countDrugs = 0;
 	cin >> n >> k >> l;
 	long long int a[n] = {0};
@@ -45,6 +45,55 @@ void solve() {
 	}
 
 	cout << "No\n";
+
+}
+
+void solve() {
+
+	long long int n, mx = -1001, k, l, countDrugs = 0;
+	cin >> n >> k >> l;
+	long long int a[n] = {0};
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+		mx = max(mx, a[i]);
+	}
+	bool f = true;
+	if (k > 0)
+	{
+		a[n - 1] = a[n - 1] + k * (l - 1);
+	}
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (a[n - 1] <= a[i])
+		{
+			f = false;
+		}
+	}
+	if (f)
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+}
+
+int solve2() {
+
+	int N, K, L; cin >> N >> K >> L;
+
+	int friend_speed, fastest = 0;
+	for (int x = 0; x < N - 1; x++) {
+		int i; cin >> i;
+		fastest = max(fastest, i);
+	}
+	cin >> friend_speed;
+
+	if (K < 0) {
+		if (friend_speed > fastest) cout << "Yes\n";
+		else cout << "No\n";
+	} else {
+		if (friend_speed + (L - 1)*K > fastest) cout << "Yes\n";
+		else cout << "No\n";
+	}
 
 }
 
