c1e2bca0598b751bba233777d87e7c6a364eab85
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-09-16 01:31:54 +0530

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,49 +1,15 @@
-#include <iostream>
 #include <bits/stdc++.h>
-#include <string>
 
 using namespace std;
-
-#define square(x) x*x
+using ll = long long;
 
 int main()
 {
-   /*string s="abababab";
-   string sm=s.substr(0,2);
-   cout << sm ;*/
-   vector <int> a;
-   a.push_back(6);
-   a.push_back(2);
-   a.push_back(1);
-   a.push_back(4);
-   a.push_back(5);
-   a.push_back(3);
-   int m=*min_element(a.begin(),a.begin()+6);
-   cout << m;
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cout << "Hello, world!" << endl;
 
 
-   /*for(auto i=a.begin(); i <= a.begin()+2;i++)
-   {
-      cout << *i << " ";
-   }*/
-  /*vector < vector < int > > Matrix(5, vector< int >(5,15));
-   for(int i=0;i< 5;i++)
-   {
-      for(int j=0;j<5;j++)
-      {
-         cout<<Matrix[i][j]<<" ";
-      }
-      cout<<endl;
-   }
-   cout<<endl;
-   vector<vector<int>> s(vector<int>(5,15));
-   for(int i = 0;i < s.size() ;i++)
-   {
-      for(int j : s[i])
-      {
-         cout << j <<" ";
-      }
-      cout<<endl;
-   }
-*/
-} 
+	return 0;
+}
