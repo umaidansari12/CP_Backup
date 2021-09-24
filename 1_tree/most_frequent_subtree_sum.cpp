d0ca07fff8340d602124ea5cb3390c3b8d7def0c
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-09-24 23:24:00 +0530

    Create most_frequent_subtree_sum.cpp

---
+++ 1_tree/most_frequent_subtree_sum.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, int> m;
+    int maxCount = 0;
+    int dfs(TreeNode* root)
+    {
+        if (!root)
+            return 0;
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+        int sum = left + right + root->val;
+        maxCount = max(maxCount, ++m[sum]);
+        return sum;
+    }
+    vector<int> findFrequentTreeSum(TreeNode* root) {
+        dfs(root);
+        vector<int> res;
+        for (auto i : m)
+        {
+            if (i.second == maxCount)
+                res.push_back(i.first);
+        }
+        return res;
+    }
+};
