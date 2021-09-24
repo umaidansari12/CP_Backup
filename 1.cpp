ccb070643965be0bab5102fb419d6f085cb1eefa
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-09-24 21:56:48 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -4,22 +4,27 @@ using namespace std;
 
 int main()
 {
-    int n,k;
-    cout << "Enter number of Elements you want to insert : ";
-    cin >> n;
-    unordered_map<int,int> a;
-    for(int i = 0; i < n ; i++)
-    {
-        cin>>k;
-        a[k]++; 
-    }
-     for (auto itr :a) { 
-        if(itr.second==1)
-         {
-             cout<<itr.first<<"\t";
-             //cout<<itr.second<<endl;
-            break;
-         }
-    } 
-    cout << endl; 
+    int a = 0;
+    if (!++a)
+        cout << "Hello";
+    else
+        cout << "Fuckoff";
+    // int n,k;
+    // cout << "Enter number of Elements you want to insert : ";
+    // cin >> n;
+    // unordered_map<int,int> a;
+    // for(int i = 0; i < n ; i++)
+    // {
+    //     cin>>k;
+    //     a[k]++;
+    // }
+    //  for (auto itr :a) {
+    //     if(itr.second==1)
+    //      {
+    //          cout<<itr.first<<"\t";
+    //          //cout<<itr.second<<endl;
+    //         break;
+    //      }
+    // }
+    // cout << endl;
 }
