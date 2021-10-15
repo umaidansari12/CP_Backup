1b340508b754be282d45879e11c8e3b0faaa70db
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-10-16 01:07:09 +0530

    Update as_far_from_land_as_possible.cpp

--- 8_graph/as_far_from_land_as_possible.cpp
+++ 8_graph/as_far_from_land_as_possible.cpp
@@ -101,3 +101,44 @@ public:
 
 	}
 };
+
+// we are doing multisource bfs
+
+class Solution {
+public:
+	int maxDistance(vector<vector<int>>& grid) {
+		queue<pair<int, int>> q;
+		int n = grid.size();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (grid[i][j] == 1) {
+					q.push({i, j});
+				}
+			}
+		}
+		//either no land is there or everywhere there is land no water  is there in both case we cant find the distance to the nearest land cell so we return -1
+		if (q.size() == 0 or q.size() == (n * n))
+			return -1;
+		int dirX[] = { -1, 0, 1, 0};
+		int dirY[] = {0, 1, 0, -1};
+		int level = -1; //we just need to count how many levels are there levels signifies distance from the current land cell to water
+		while (!q.empty()) {
+			level++;
+			int size = q.size();
+			while (size--) {
+				pair<int, int> curr = q.front();
+				q.pop();
+				for (int i = 0; i < 4; i++) {
+					int nextRow = curr.first + dirX[i];
+					int nextCol = curr.second + dirY[i];
+					if (nextRow<0 or nextCol<0 or nextRow >= n or nextCol >= n or grid[nextRow][nextCol] == 1)
+						continue;
+					q.push({nextRow, nextCol});
+					grid[nextRow][nextCol] = 1;
+				}
+			}
+		}
+		return level;
+
+	}
+};
