8bf1b92d71578c4599da60bb68a279cf51c78a8b
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-10-16 21:46:39 +0530

    Update k_largest_elements.cpp

--- 22_heaps/k_largest_elements.cpp
+++ 22_heaps/k_largest_elements.cpp
@@ -15,6 +15,34 @@ vector<int> kLargest(int arr[], int n, int k)
 vector<int> kLargest(int arr[], int n, int k)
 {
     // code here
+    vector<int> ans;
+    sort(arr, arr + n);
+    for (int i = n - 1; i >= n - k; i--)
+        ans.push_back(arr[i]);
+    return ans;
+}
+vector<int> kLargest(int arr[], int n, int k)
+{
+    // code here
+    priority_queue<int, vector<int>, greater<int>> q;
+    for (int i = 0; i < n; i++) {
+        q.push(arr[i]);
+    }
+    vector<int> r;
+    int times = n - k;
+    while (times--) {
+        q.pop();
+    }
+    while (k--) {
+        r.push_back(q.top());
+        q.pop();
+    }
+    reverse(r.begin(), r.end());
+    return r;
+}
+vector<int> kLargest(int arr[], int n, int k)
+{
+    // code here
     vector<int> r;
     priority_queue<int, vector<int>> p;
     for (int i = 0; i < n; i++)
