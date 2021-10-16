13e44c81e899dc749de3f729f13b0667d67cc35b
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-10-16 21:22:06 +0530

    Create k_largest_elements.cpp

---
+++ 22_heaps/k_largest_elements.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+vector<int> kLargest(int arr[], int n, int k)
+{
+    // code here
+    vector<int> ans;
+    sort(arr, arr + n, greater<int>());
+    for (int i = 0; i < k; i++)
+        ans.push_back(arr[i]);
+    return ans;
+}
+vector<int> kLargest(int arr[], int n, int k)
+{
+    // code here
+    vector<int> r;
+    priority_queue<int, vector<int>> p;
+    for (int i = 0; i < n; i++)
+        p.push(arr[i]);
+    while (k--)
+    {
+        r.push_back(p.top());
+        p.pop();
+    }
+    return r;
+}
+
+// { Driver Code Starts.
+vector<int> kLargest(int arr[], int n, int k);
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+
+        int arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+
+        vector<int> result = kLargest(arr, n, k);
+        for (int i = 0; i < result.size(); ++i)
+            cout << result[i] << " ";
+        cout << endl;
+
+    }
+    return 0;
+}
+// } Driver Code Ends
