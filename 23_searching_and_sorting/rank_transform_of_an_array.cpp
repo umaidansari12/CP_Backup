a29c3e8596f6a7cbd80cf0ea0f2aa8e616d6967e
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-10-25 17:53:36 +0530

    Update rank_transform_of_an_array.cpp

--- 23_searching_and_sorting/rank_transform_of_an_array.cpp
+++ 23_searching_and_sorting/rank_transform_of_an_array.cpp
@@ -17,3 +17,17 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> A(arr);
+        sort(A.begin(), A.end());
+        unordered_map<int, int> rank;
+        for (int& a : A)
+            rank.emplace(a, rank.size() + 1);
+        for (int i = 0; i < A.size(); ++i)
+            A[i] = rank[arr[i]];
+        return A;
+    }
+};
