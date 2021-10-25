79884d9ad6a9fd9edcf0c506e93ded0408b30bd6
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-10-26 01:11:06 +0530

    Create convert_to_roman_number.cpp

---
+++ 9_maths_and_number_theory/convert_to_roman_number.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+public:
+    string convertToRoman(int n) {
+        // code hereo
+        int codeNum[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string code[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        string s = "";
+        for (int i = 0; i < 13; i++) {
+            while (n >= codeNum[i]) {
+                s += code[i];
+                n -= codeNum[i];
+            }
+        }
+        return s;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.convertToRoman(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
