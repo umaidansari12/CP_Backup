a693ae56d45edaba2d093a9b5f9b0c1eb4ba741d
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-10-26 01:11:25 +0530

    Update roman_to_integer.cpp

--- 9_maths_and_number_theory/roman_to_integer.cpp
+++ 9_maths_and_number_theory/roman_to_integer.cpp
@@ -45,3 +45,43 @@ public:
         return r;
     }
 };
+
+class Solution {
+public:
+    int value(char ch) {
+        if (ch == 'I')
+            return 1;
+        else if (ch == 'V')
+            return 5;
+        else if (ch == 'X')
+            return 10;
+        else if (ch == 'L')
+            return 50;
+        else if (ch == 'C')
+            return 100;
+        else if (ch == 'D')
+            return 500;
+        else if (ch == 'M')
+            return 1000;
+        return 0;
+    }
+    int romanToInt(string s) {
+        int n = s.size(), result = 0;
+        for (int i = 0; i < n; i++) {
+            int value1 = value(s[i]);
+            if (i + 1 < n) {
+                int value2 = value(s[i + 1]);
+                if (value2 > value1) {
+                    result = result + (value2 - value1);
+                    i++;
+                }
+                else
+                    result += value1;
+            }
+            else {
+                result += value1;
+            }
+        }
+        return result;
+    }
+};
