45b0e4212f11dbc1514e26d48aaabb054eb459ab
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2021-11-26 01:08:56 +0530

    Update find_the_highest_number.cpp

--- 4_array/find_the_highest_number.cpp
+++ 4_array/find_the_highest_number.cpp
@@ -17,6 +17,37 @@ public:
         }
         return _max;
     }
+    int findPeakElement(vector<int>& arr)
+    {
+        // Code here.
+        //second approach is to use binary search on the given array as it is following the order of increasing then decreasing
+
+        int n = arr.size();
+        int low = 0 , high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // if there are only two elements left in the partition and first element is greater than the second element then return the first element
+            if ((high == low + 1) and arr[low] >= arr[high])
+                return arr[low];
+            // if there are only two elements left in the partition and first element is less than the second element then return the second element
+            if ((high == low + 1) and arr[low] < arr[high])
+                return arr[high];
+            // if the adjacent elements are lesser than the current element then return the current element
+            if (arr[mid] > arr[mid - 1] and arr[mid] > arr[mid + 1])
+                return arr[mid];
+            // if the current element is less than the next element then the element lies on the right side move the low pointer
+            else if ((mid + 1) < n and arr[mid] < arr[mid + 1])
+            {
+                low = mid + 1;
+            }
+            // if the current element is greater than the next element then the element lies on the left side move the right pointer
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return 0;
+    }
 };
 
 
