85f18a5f8b288e69dfaa045dc5d73fa63df735d4
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2022-02-07 01:39:25 +0530

    Create B_equal_card_game.cpp

---
+++ codechef/starter_22_div_3/B_equal_card_game.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main()
+{
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t;
+	cin >> t;
+	while (t--) {
+		int k;
+		cin >> k;
+		cout << (52 % k) << endl;
+	}
+
+
+	return 0;
+}
