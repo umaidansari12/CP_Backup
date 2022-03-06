58caec9e6719b605610f4d812250a0bec39beb75
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2022-03-06 21:23:31 +0530

    Create minimum_sum_of_subarray.cpp

---
+++ 4_array/minimum_sum_of_subarray.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main()
+{
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		int a[n] = {0};
+		for (int i = 0; i < n; i++)
+			cin >> a[i];
+
+	}
+
+
+	return 0;
+}
