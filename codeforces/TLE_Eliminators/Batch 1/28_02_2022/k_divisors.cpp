a6cafdd3b5671653e946a7287096aed884a2f78e
Mohammad Umaid Ansari <umaidansari123@gmail.com>
2022-03-07 01:31:48 +0530

    Create k_divisors.cpp

---
+++ codeforces/TLE_Eliminators/Batch 1/28_02_2022/k_divisors.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main()
+{
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t = 1;
+	//cin>>t;
+	while (t--) {
+		int n;
+		cin >> n;
+		/*for (int i = 1; i <= n; i++) {
+			if ((n % i) == 0) {
+				cout << i << "\n";
+			}
+		}*/
+		/*for (int i = 1; i * i <= n; i++) {
+			if ((n % i) == 0) {
+				cout << i << "\n";
+			}
+		}*/
+
+		cout << n << endl;
+	}
+
+	return 0;
+}
